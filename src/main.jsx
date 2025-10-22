0d27d7d27cd7079ed15981dbc0b3a4f059c8bd7e
oleksii <leizerovich1321@gmail.com>
2025-10-22 12:42:43 +0300

    init + i18n useTranslation added

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import './index.css'
+import App from './App.jsx'
+
+i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        supportedLngs: ['en', 'uk'],
+        fallbackLng: 'en',
+        detection: {
+            order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+            caches: ['cookie'],
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/translation.json', // Шлях до файлів перекладу
+        },
+        react: {
+            useSuspense: false,
+        },
+    });
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
